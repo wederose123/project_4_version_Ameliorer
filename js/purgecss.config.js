5bb3b48881196f580a04085cc6577cdf9ec48164
Mickael Louis <mickamickael93@gmail.com>
2022-07-21 19:28:15 +0200

    reogarnitation du repertoir du projet

---
+++ js/purgecss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['index.html'],
+    css: ['./css/bootstrap.css',
+        './css/et-line.css',
+        './css/font-awesome.css',
+        './style.css'],
+    keyframes: true,
+    variables: true,
+
+}
